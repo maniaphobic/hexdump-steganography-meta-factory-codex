b598be2fdd856e17e210cfabeaeddc6ebdf1c396
mANIA pHOBIC <git@maniaphobic.org>
2012-03-13 15:03:28 -0700

    I added a Python script implementing simple XOR encryption.

---
+++ codex/cryptxor.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#________________________________________
+# Date created:			Monday, 12 March 2012
+#
+
+#
+#
+#
+
+from base64 import b64encode, b64decode
+from itertools import izip, cycle
+import sys
+
+#
+#
+#
+
+def decrypt(key, ciphertext):
+    return(''.join([chr(ord(x) ^ ord(y)) for(x,y) in izip(b64decode(ciphertext), cycle(key))]))
+
+def encrypt(key, plaintext):
+    return(b64encode(''.join([chr(ord(x) ^ ord(y)) for(x,y) in izip(plaintext, cycle(key))])))
+
+#
+#
+#
+
+key_file			= open(sys.argv[1])
+key				= key_file.readlines()
+key_file.close()
+
+plaintext_file			= open(sys.argv[2])
+plaintext			= plaintext_file.readlines()
+plaintext_file.close()
+
+#
+#
+#
+
+print plaintext; print "\n"
+ciphertext = encrypt(key, plaintext)
+print ciphertext; print "\n"
+plaintext = decrypt(key, ciphertext)
+print plaintext
+
+
